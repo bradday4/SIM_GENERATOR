bfbb42edd919aeb691aa5a77cea2b45d9d7b31ab
Brad <bradday4@gmail.com>
2021-07-31 22:31:36 -0400

    reorder imports

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 from distutils.core import setup
+
 import setuptools  # noqa
 
 BASEPKG = ["numpy", "scipy", "pillow", "pyfiglet"]
--- test/test_pattern_gen.py
+++ test/test_pattern_gen.py
@@ -1,2 +1,3 @@
 import pytest
+
 from sim_generator.pattern_gen import SimGenerator
